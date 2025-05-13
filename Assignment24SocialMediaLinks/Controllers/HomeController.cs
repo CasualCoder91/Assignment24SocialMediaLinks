2af4d954bea09a8fdb6ddde0c6ae31072c33c4a5
Alarich Herzner <alarich2@hotmail.com>
2025-05-13 22:21:41 +0200

    Add project files.

---
+++ Assignment24SocialMediaLinks/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment24SocialMediaLinks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            SocialMediaLinksOptions? options = _configuration.GetSection("SocialMediaLinks").Get<SocialMediaLinksOptions>();
+
+            ViewBag.SocialMediaLinksOptions = options;
+
+            return View();
+        }
+    }
+}
